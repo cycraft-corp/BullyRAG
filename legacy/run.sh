32488f786df7519402815f3cf48c37fcc2a9d2a4
Lily <yenshanchen.tw@gmail.com>
2024-11-11 13:50:46 +0800

    delete legacy

--- legacy/run.sh
+++
@@ -1,6 +0,0 @@
-python main.py \
-    --title "Evaluator_Tool" \
-    --path-to-data sample_data/qa-pair-2024.json \
-    --path-to-result results \
-    --model-interface OpenAI \
-    --model gpt-3.5-turbo
